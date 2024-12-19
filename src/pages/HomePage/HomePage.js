8a4d8c0993619ca8e8d608d9870dda298dca9a3b
YoussufAhmedEE <110684819+YoussufAhmedEE@users.noreply.github.com>
2024-12-19 20:09:49 +0200

    sign up small update :))

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -22,7 +22,7 @@ const HomePage = () => {
             Login
           </Button>
           <Button href="/signup" color="inherit">
-            Sing Up
+            Sign Up
           </Button>
         </Toolbar>
       </AppBar>
