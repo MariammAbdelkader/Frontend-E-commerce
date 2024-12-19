75fe6e0e92a90b69cbdaf6c1ce7d3ef5987aeb3c
YoussufAhmedEE <110684819+YoussufAhmedEE@users.noreply.github.com>
2024-12-19 12:11:51 +0200

    moving the project to this Repo

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import StorePage from "../pages/StorePage/StorePage";
+import ChatbotPage from "../pages/ChatbotPage/ChatbotPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import SignUpPage from "../pages/SignupPage/SignupPage";
+import UploadCSVPage from "../pages/UploadCsvPage/UploadCsvPage";
+import HomePage from "../pages/HomePage/HomePage";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/chatbot" element={<ChatbotPage />} />
+        <Route path="/store" element={<StorePage />} />
+        <Route path="/upload" element={<UploadCSVPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
